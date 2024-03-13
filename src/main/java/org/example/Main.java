2baf5c45517ac36370012be5908baa06318cc527
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-03-13 20:19:44 +0300

    Refactor structure

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LogarithmicFunctions logarithmicFunctions = new LogarithmicFunctions(0.00001);
+        System.out.println(logarithmicFunctions.ln(2));
+
+        /* FunctionSystem functionSystem = new FunctionSystem();
+        System.out.println(functionSystem.calculate(-2.1, 0.001));*/
     }
+
+
 }
