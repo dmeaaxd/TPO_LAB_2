4c20be03aa9421922f1bb17a44c1cbaf80267c3b
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-03-13 20:00:01 +0300

    Refactor structure

--- src/main/java/org/example/FunctionSystem.java
+++ src/main/java/org/example/FunctionSystem.java
@@ -1,8 +1,20 @@
 package org.example;
 
 public class FunctionSystem {
-    double calculate(double x){
-        if (x <= 0):
-            return ((cot(x) / tan(x)) ** 2)
+    private final double e = 2.718281828459045235;  // Число эйлера
+
+    public double calculate(double x, double precision){
+        TrigonometricFunctions tf = new TrigonometricFunctions(precision);
+        LogarithmicFunctions lf = new LogarithmicFunctions(precision);
+
+        if (x <= 0){
+            return Math.pow((tf.cot(x) / tf.tan(x)), 2);
+        }
+        else{
+            double first = Math.pow(Math.pow(lf.log(5, x), 2) / lf.log(3, x), 3)
+                    / (lf.log(10, x) * lf.log(e, x) * lf.log(10, x));
+            double second = Math.pow(Math.pow(lf.log(10, x), 3) * lf.log(10, x), 2) + (lf.log(10, x) * lf.log(3, x));
+            return first * second;
+        }
     }
 }
--- src/main/java/org/example/TrigonometricFunctions.java
+++ src/main/java/org/example/TrigonometricFunctions.java
@@ -39,26 +39,4 @@ public class TrigonometricFunctions {
     public double cot(double x) {
         return 1 / tan(x);
     }
-
-    public double ln(double x) {
-        if (x <= 0) {
-            return Double.NaN;
-        }
-        double result = x - 1;
-        double term = x - 1;
-        int n = 2;
-        while (Math.abs(term) > precision) {
-            term = Math.pow(x - 1, n) / n;
-            result += term;
-            n++;
-        }
-        return result;
-    }
-
-    public double log(double base, double x) {
-        if (base <= 0 || x <= 0) {
-            return Double.NaN;
-        }
-        return ln(x) / ln(base);
-    }
 }
