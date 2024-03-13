9635f4bb93923b062b7fd948f767bd1992b554dd
Maksim Ushakov <dmeaaxd@gmail.com>
2024-03-13 22:37:15 +0300

    fix handler

--- src/main/java/org/example/CsvHandler.java
+++ src/main/java/org/example/CsvHandler.java
@@ -1,6 +1,5 @@
 package org.example;
 
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -8,40 +7,56 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class CsvHandler {
 
     private static FileWriter writer;
 
     public static void openFile(String csvFile) throws IOException {
-        writer = new FileWriter(csvFile, false); // Открытие файла для перезаписи
+        try {
+            writer = new FileWriter(csvFile, false);
+        } catch (IOException e) {
+            System.out.println("Ошибка при открытии файла: " + e.getMessage());
+            throw e;
+        }
     }
 
     public static void writeToCsv(String func, double X, double res) throws IOException {
         if (writer == null) {
-            System.out.println("Файл не открыт");
+            throw new IOException("Файл не открыт");
         }
-        writer.append(func + "," + X + "," + res + "\n");
+        try {
+            writer.append(func + "," + X + "," + res + "\n");
+        } catch (IOException e) {
+            System.out.println("Ошибка записи в файл: " + e.getMessage());
+            throw e;
+        }
     }
 
     public static String[] readCsv(String csvFile) throws IOException {
-        List<String> lines = new ArrayList<>(); // Создаем список для хранения строк из файла
+        List<String> lines = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             String line;
             while ((line = br.readLine()) != null) {
-                lines.add(line); // Добавляем каждую строку из файла в список
+                lines.add(line);
             }
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения файла: " + e.getMessage());
+            throw e;
         }
 
-        return lines.toArray(new String[0]); // Преобразуем список в массив строк и возвращаем его
+        return lines.toArray(new String[0]);
     }
 
     public static void closeAndSave() throws IOException {
         if (writer != null) {
-            writer.flush();
-            writer.close();
+            try {
+                writer.flush();
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("Ошибка при закрытии файла: " + e.getMessage());
+                throw e;
+            }
         }
     }
-
 }
