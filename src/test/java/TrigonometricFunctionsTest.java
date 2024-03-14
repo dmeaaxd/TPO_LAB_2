2b4741f3c7125f540fd18183a492bbc3c99bf33e
Daniil Kalinin <dan_kalinin_03@mail.ru>
2024-03-14 17:24:53 +0300

    Add tan tests

--- src/test/java/TrigonometricFunctionsTest.java
+++ src/test/java/TrigonometricFunctionsTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class TrigonometricFunctionsTest {
+    private static final double DELTA = 1e-10;
+
     // Тестируем sin
     @Test
     public void testSin_zero(){
