d59cc03e4cdb130c703eb35546d1dcf163339721
Maksim Ushakov <dmeaaxd@gmail.com>
2024-03-14 14:13:35 +0300

    final fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        FunctionSystem functionSystem = new FunctionSystem();
-        System.out.println(functionSystem.calculate((Double) null, 0.001));
+
     }
 }
